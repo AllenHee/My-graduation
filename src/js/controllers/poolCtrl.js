48b6262be2600f634af94f592c1904e67a3172a2
He Wei <hw961919531@outlook.com>
2016-05-09 21:06:32 +0800

    还差最后两个表格

--- src/js/controllers/poolCtrl.js
+++ src/js/controllers/poolCtrl.js
@@ -1,16 +1,7 @@
 export default function poolCtrl() {
   $('.whiteCir').on('click', function () {
-    $('.current').find('.whiteCir').css('animation', 'whiteCirShow 0.5s forwards');
-    $('.current').find('.ap_cont_item_detail').animate({height: 0}, 500);
-    $('.current').animate({height: '30px'}, 500);
     var that = $(this);
     var index = that.parent('.ap_cont_item').index();
-    that.css('animation', 'whiteCirHide 0.5s forwards');
-    setTimeout(function () {
-      // that.parent('.ap_cont_item').animate({ height: '116px' }, 500);
-      that.parent('.ap_cont_item').addClass('c');
-      that.parent('.ap_cont_item').find('.ap_cont_item_detail').animate({ height: '116px' }, 500);
       $('.ap_cont_item').removeClass('current').eq(index).addClass('current');
-    }, 500);
   })
 }
